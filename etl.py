dcc22850624150a2ff0605de474d3bf22f7ff172
basketballrelativity <patmcfarla@gmail.com>
2025-11-29 12:49:43 -0500

    checking in untested field goal logic

--- etl.py
+++ etl.py
@@ -9,41 +9,6 @@ import pandas as pd
 
 import data
 
-def create_turnover_play(play: pd.Series, home_id: int):
-    """ This helper function allocates the team
-    turning the ball over to the offense, the
-    other team to the defense, and points to zero
-
-    Args:
-        play (pd.Series): Pandas series for an
-            individual play
-        home_id (int): Integer identifier for the home
-            team
-
-    Returns:
-        play_df (pd.DataFrame): One-row DataFrame
-            in RAPM format
-    """
-
-    # Player 1 corresponds to the player committing
-    # the turnover, so his team is on offense
-    if play["player1_team_id"] == home_id:
-        off_prefix = "home_"
-        def_prefix = "visitor_"
-    else:
-        off_prefix = "visitor_"
-        def_prefix = "home_"
-    
-    # Initialize play DataFrame with 0 points
-    play_df = pd.DataFrame({"points": [0], "eventnum": [play["eventnum"]]})
-
-    # Loop through to add 
-    for player_number in range(1, 6):
-        play_df["offensive_player_" + str(player_number)] = play[off_prefix + "player_" + str(player_number)]
-        play_df["defensive_player_" + str(player_number)] = play[def_prefix + "player_" + str(player_number)]
-
-    return play_df
-
 
 def determine_rebound_type(play: pd.Series, pbp_df: pd.DataFrame, last_possession: int):
     """ This function determines whether a particular rebound
@@ -181,18 +146,120 @@ def determine_scoring_type(play: pd.Series, pbp_df: pd.DataFrame, last_possessio
             data with home/away player IDs
         last_possession (int): Event number of the end of
             the last possession
+
+    Returns:
+        points (int): Number of points scored on the possession
+        remove_shot (int): Event number of the field goal to remove
+            from the RAPM-style accounting for field goals
     """
 
     # We're looking for the shot immediately preceding the rebound
     shot_event = play["eventnum"]
+    remove_shot = -1
     if play["eventmsgtype"] == 1:
         if ("3PT" in str(play["homedescription"])) or ("3PT" in str(play["visitordescription"])):
             points = 3
         else:
             points = 2
+    else:
+        poss_df = pbp_df[
+            (
+                pbp_df["eventnum"] > last_possession
+            ) &
+            (
+                pbp_df["eventnum"] <= shot_event
+            )
+        ]
+        made_free_throw_df = poss_df[
+                (
+                    poss_df["eventmsgtype"]==3
+                ) &
+                (
+                    poss_df["player1_id"] == play["player1_id"]
+                ) &
+                (
+                    (
+                        ~poss_df["homedescription"].str.contains("MISS", na=False)
+                    ) &
+                    (
+                        ~poss_df["visitordescription"].str.contains("MISS", na=False)
+                    )
+                )
+            ]
+        made_shot_df = poss_df[
+            (
+                poss_df["eventmsgtype"]==1
+            ) &
+            (
+                poss_df["player1_id"] == play["player1_id"]
+            )
+        ]
+        if len(made_shot_df) > 0:
+            made_shot = made_shot_df.iloc[-1]
+            if ("3PT" in str(made_shot["homedescription"])) or ("3PT" in str(made_shot["visitordescription"])):
+                points = 3
+            else:
+                points = 2
+            
+            remove_shot = made_shot["eventnum"]
+        else:
+            points = 0
+        
+        points += len(made_free_throw_df)
+
+    return points, remove_shot
     
 
-def create_rebound_play(play: pd.Series, home_id: int, points: int):
+def create_turnover_play(play: pd.Series, home_id: int, visitor_id: int):
+    """ This helper function allocates the team
+    turning the ball over to the offense, the
+    other team to the defense, and points to zero
+
+    Args:
+        play (pd.Series): Pandas series for an
+            individual play
+        home_id (int): Integer identifier for the home
+            team
+        visitor_id (int): Integer identifier for the visiting
+            team
+
+    Returns:
+        play_df (pd.DataFrame): One-row DataFrame
+            in RAPM format
+    """
+
+    # Player 1 corresponds to the player committing
+    # the turnover, so his team is on offense
+    if play["player1_team_id"] == home_id:
+        off_prefix = "home_"
+        def_prefix = "visitor_"
+        off_id = home_id
+        def_id = visitor_id
+    else:
+        off_prefix = "visitor_"
+        def_prefix = "home_"
+        off_id = visitor_id
+        def_id = home_id
+    
+    # Initialize play DataFrame with 0 points
+    play_df = pd.DataFrame(
+        {
+            "points": [0],
+            "eventnum": [play["eventnum"]],
+            "offensive_team_id": [off_id],
+            "defensive_team_id": [def_id]
+        }
+    )
+
+    # Loop through to add 
+    for player_number in range(1, 6):
+        play_df["offensive_player_" + str(player_number)] = play[off_prefix + "player_" + str(player_number)]
+        play_df["defensive_player_" + str(player_number)] = play[def_prefix + "player_" + str(player_number)]
+
+    return play_df
+
+
+def create_rebound_play(play: pd.Series, home_id: int, visitor_id: int, points: int):
     """ This helper function allocates the team
     rebounding the ball over to the defense, the
     other team to the offense, and points to zero
@@ -201,6 +268,8 @@ def create_rebound_play(play: pd.Series, home_id: int, points: int):
         play (pd.Series): Pandas series for an
             individual play
         home_id (int): Integer identifier for the home
+            team
+        visitor_id (int): Integer identifier for the visiting
             team
         points (int): Number of points scored on the
             possession
@@ -215,12 +284,74 @@ def create_rebound_play(play: pd.Series, home_id: int, points: int):
     if play["player1_team_id"] == home_id:
         off_prefix = "visitor_"
         def_prefix = "home_"
+        off_id = visitor_id
+        def_id = home_id
     else:
         off_prefix = "home_"
         def_prefix = "visitor_"
+        off_id = home_id
+        def_id = visitor_id
     
-    # Initialize play DataFrame with 0 points
-    play_df = pd.DataFrame({"points": [points], "eventnum": [play["eventnum"]]})
+    # Initialize play DataFrame with points
+    play_df = pd.DataFrame(
+        {
+            "points": [points],
+            "eventnum": [play["eventnum"]],
+            "offensive_team_id": [off_id],
+            "defensive_team_id": [def_id]
+        }
+    )
+
+    # Loop through to add 
+    for player_number in range(1, 6):
+        play_df["offensive_player_" + str(player_number)] = play[off_prefix + "player_" + str(player_number)]
+        play_df["defensive_player_" + str(player_number)] = play[def_prefix + "player_" + str(player_number)]
+
+    return play_df
+
+
+def create_scoring_play(play: pd.Series, home_id: int, visitor_id: int, points: int):
+    """ This helper function allocates the team
+    scoring the ball over to the offense, the
+    other team to the defense, and points to points
+
+    Args:
+        play (pd.Series): Pandas series for an
+            individual play
+        home_id (int): Integer identifier for the home
+            team
+        visitor_id (int): Integer identifier for the visiting
+            team
+        points (int): Number of points scored on the
+            possession
+
+    Returns:
+        play_df (pd.DataFrame): One-row DataFrame
+            in RAPM format
+    """
+
+    # Player 1 corresponds to the player scoring
+    # the ball, so his team is on offense
+    if play["player1_team_id"] == home_id:
+        off_prefix = "home_"
+        def_prefix = "visitor_"
+        off_id = home_id
+        def_id = visitor_id
+    else:
+        off_prefix = "visitor_"
+        def_prefix = "home_"
+        off_id = visitor_id
+        def_id = home_id
+    
+    # Initialize play DataFrame with points
+    play_df = pd.DataFrame(
+        {
+            "points": [points],
+            "eventnum": [play["eventnum"]],
+            "offensive_team_id": [off_id],
+            "defensive_team_id": [def_id]
+        }
+    )
 
     # Loop through to add 
     for player_number in range(1, 6):
@@ -254,13 +385,14 @@ def isolate_possessions(pbp_df: pd.DataFrame, game_id: int):
     # Pull game information
     game_df = data.ingest_game_data(game_id)
     home_id = game_df['home_team_id'].values[0]
+    visitor_id = game_df['visitor_team_id'].values[0]
 
     # Loop through each play
     for index, row in pbp_df.iterrows():
         # This is a turnover, so it's fairly straightforward
         # to allocate offense, defense, and points
         if row["eventmsgtype"] == 5:
-            play_df = create_turnover_play(row, home_id)
+            play_df = create_turnover_play(row, home_id, visitor_id)
             last_possession = row["eventnum"]
 
             poss_df = pd.concat([poss_df, play_df])
@@ -269,7 +401,7 @@ def isolate_possessions(pbp_df: pd.DataFrame, game_id: int):
         elif row["eventmsgtype"] == 4:
             def_rebound, points, remove_shot = determine_rebound_type(row, pbp_df, last_possession)
             if def_rebound:
-                play_df = create_rebound_play(row, home_id, points)
+                play_df = create_rebound_play(row, home_id, visitor_id, points)
                 last_possession = row["eventnum"]
                 poss_df = poss_df[poss_df["event_num"] != remove_shot]
                 poss_df = pd.concat([poss_df, play_df])
@@ -277,6 +409,10 @@ def isolate_possessions(pbp_df: pd.DataFrame, game_id: int):
             # Reset rebound boolean
             def_rebound = False
         elif row["eventmsgtype"] in [1, 3]:
-            play_df = determine_scoring_type(row, pbp_df, last_possession)
+            points, remove_shot = determine_scoring_type(row, pbp_df, last_possession)
+            play_df = create_scoring_play(row, home_id, visitor_id, points)
+            last_possession = row["eventnum"]
+            poss_df = poss_df[poss_df["event_num"] != remove_shot]
+            poss_df = pd.concat([poss_df, play_df])
 
     return poss_df
